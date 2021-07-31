77224470316c1b272f28cc9cae6c71808341e058
Kasey <mccormick.kasey@gmail.com>
2021-07-30 18:49:49 -0600

    dashboard component

--- garden-minder/src/app/app-routing.module.ts
+++ garden-minder/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  {
+    path: '',
+component: DashboardComponent  },
+  // {
+  //   path: 'user-profile',
+  //   loadChildren: () => import('@my-streetparking-nx/xplat/web/features').then((m) => m.UserProfileModule),
+  // },
+  {
+    path: '**',
+    redirectTo: 'error/404',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
